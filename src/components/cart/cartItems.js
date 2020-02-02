fc92af4b420b0db13d8eaf27d43e81d1726f0acb
Van <vancreative@gmail.com>
2020-02-02 17:50:33 +1300

    implemented individual item count, price and total price of the cart

--- src/components/cart/cartItems.js
+++ src/components/cart/cartItems.js
@@ -7,9 +7,7 @@ class CartItems extends Component {
     console.log(cartItems.length);
     return(
       <div>
-        <p className="text-center cartIcon"> <FaCartPlus />
-          <span className="pl-1">Cart</span>
-        </p>
+        <p className="text-center cartIcon"> <FaCartPlus /><span className="pl-1"> Cart</span> </p>
         { cartItems.length === 0 ? "Your cart is empty"
           : <div className="cartLength">
             <span className="rounded px-2 py-1 ">
@@ -23,16 +21,27 @@ class CartItems extends Component {
                         <div className="clearfix">
                           <div className="float-left">
                             <b>{item.name}</b>
+                            <p>{item.count} X {(item.price).toFixed(2)}</p>
                           </div>
 
-                          <div className="float-left">
-
+                          <div className="float-right">
+                            delete button
                           </div>
                         </div>
                       </li>
                     ))}
 
                   </ul>
+                  <div className="clearfix">
+                    <div className="float-left">
+                      <b>Total</b>:
+                    </div>
+                    <div className="float-right">
+                      ${(cartItems.reduce((a,c) => (a + c.price * c.count), 0)).toFixed(2)}
+                    </div>
+                  </div>
+
+
               </div>
             }
       </div>
